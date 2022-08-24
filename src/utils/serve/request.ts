600cfbea92f5f51fbf1a2de6c7f38dc3e78c17f7
huajian <huajian@corpmail.com>
2022-08-24 17:18:27 +0800

    updata: newfiles

---
+++ src/utils/serve/request.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const server = axios.create({
+    baseURL: `http://localhost:5173`,
+});
+
+//请求拦截器
+server.interceptors.request.use(
+    (config) => {
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+//响应拦截器
+server.interceptors.response.use(
+    (res) => {
+        if (res.status !== 200) {
+            return Promise.reject(new Error(`response code: ${res.status}`));
+        }
+        return res;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export { server };
