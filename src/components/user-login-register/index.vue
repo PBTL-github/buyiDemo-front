d7fc0177fad06bf9f1161ec0814551d9071dd48e
PBTL <huajian@linkgo.ltd>
2022-08-31 17:00:35 +0800

    updata: templogin

--- src/components/user-login-register/index.vue
+++ src/components/user-login-register/index.vue
@@ -10,83 +10,83 @@ const LoginTitle = ref("登入");
 const formLabelWidth = "100px";
 
 const formData = reactive({
-    basic: {
-        username: "",
-        password: "",
-    },
+  basic: {
+    username: "",
+    password: "",
+  },
 });
 
 const handleDialogFlag = (flag: boolean) => {
-    store.commit("SET_DIALOG_FLAG", flag);
+  store.commit("SET_DIALOG_FLAG", flag);
 };
 watch(
-    () => store.state.dialogFormFlag,
-    () => {
-        dialogFormVisible.value = store.getters.getDialogFormFlag;
-    }
+  () => store.state.dialogFormFlag,
+  () => {
+    dialogFormVisible.value = store.getters.getDialogFormFlag;
+  }
 );
 
 const submitBtn = () => {
-    if (LoginTitle.value == "登入") {
-        Api.userLogin.userControl(formData.basic, "login").then((res) => {
-            alert(res.data.message);
-        });
-    } else if (LoginTitle.value == "注册") {
-        Api.userLogin.userControl(formData.basic, "register").then((res) => {
-            alert(res.data.message);
-        });
-    }
-    handleDialogFlag(false);
+  if (LoginTitle.value == "登入") {
+    Api.userLogin.userControl(formData.basic, "login").then((res) => {
+      alert(res.data.message);
+    });
+  } else if (LoginTitle.value == "注册") {
+    Api.userLogin.userControl(formData.basic, "register").then((res) => {
+      alert(res.data.message);
+    });
+  }
+  handleDialogFlag(false);
 };
 watch(
-    () => store.state.LoginTitle,
-    () => {
-        LoginTitle.value = store.getters.getLoginTitle;
-    }
+  () => store.state.LoginTitle,
+  () => {
+    LoginTitle.value = store.getters.getLoginTitle;
+  }
 );
 </script>
 
 <template>
-    <el-dialog
-        v-model="dialogFormVisible"
-        :title="LoginTitle"
-        width="480px"
-        center
-        @close="handleDialogFlag(false)"
-    >
-        <el-form :model="formData.basic">
-            <el-form-item label="用户名：" :label-width="formLabelWidth">
-                <el-input
-                    type="text"
-                    v-model="formData.basic.username"
-                    autocomplete="off"
-                />
-            </el-form-item>
-            <el-form-item label="密　码：" :label-width="formLabelWidth">
-                <el-input
-                    type="text"
-                    v-model="formData.basic.password"
-                    autocomplete="off"
-                />
-            </el-form-item>
-        </el-form>
-        <template #footer>
-            <span class="dialog-footer">
-                <el-button type="primary" @click="submitBtn">确定</el-button>
-                <el-button @click="handleDialogFlag(false)">取消</el-button>
-            </span>
-        </template>
-    </el-dialog>
+  <el-dialog
+    v-model="dialogFormVisible"
+    :title="LoginTitle"
+    width="480px"
+    center
+    @close="handleDialogFlag(false)"
+  >
+    <el-form :model="formData.basic">
+      <el-form-item label="用户名：" :label-width="formLabelWidth">
+        <el-input
+          type="text"
+          v-model="formData.basic.username"
+          autocomplete="off"
+        />
+      </el-form-item>
+      <el-form-item label="密　码：" :label-width="formLabelWidth">
+        <el-input
+          type="text"
+          v-model="formData.basic.password"
+          autocomplete="off"
+        />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button type="primary" @click="submitBtn">确定</el-button>
+        <el-button @click="handleDialogFlag(false)">取消</el-button>
+      </span>
+    </template>
+  </el-dialog>
 </template>
 
 <style lang="scss" scoped>
 .el-button--text {
-    margin-right: 15px;
+  margin-right: 15px;
 }
 .el-input {
-    width: 300px;
+  width: 300px;
 }
 .dialog-footer button:first-child {
-    margin-right: 10px;
+  margin-right: 10px;
 }
 </style>
